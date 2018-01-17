f3eebdeec02afcac4b0beaea96d37edadaa28387
Atif <you@example.com>
2018-01-17 11:10:01 +0500

    added filter.js in lib

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-module.exports = (process && process.env && process.env.FILTR_COV)
-  ? require('./lib-cov/loopback-advance-filters')
-  : require('./lib/loopback-advance-filters')
+module.exports = (process && process.env && process.env.FILTR_COV) ? require('./lib-cov/filter') : require('./lib/filter')
